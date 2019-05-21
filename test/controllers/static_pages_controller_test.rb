b0066fb515acb2f7fe82971d43dcd39a30e15245
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-05-21 08:22:40 +0000

    Add title test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url #static_pages/homeにアクセスしたら
     assert_response :success  #レスポンスが成功するはず
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"  #以下のHTMLが存在するはず
   end
 
   test "should get help" do
     get static_pages_help_url  #static_pages/helpにアクセスしたら
     assert_response :success  #レスポンスが成功するはず
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"  #以下のHTMLが存在するはず
   end
 
   test "should get about" do
     get static_pages_about_url  #static_pages/aboutにアクセスしたら
     assert_response :success  #レスポンスが成功するはず
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"  #以下のHTMLが存在するはず
   end
 
 end
