c239be699db420c7e494cfbcab711c66ae4f09ec
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-05-21 06:48:04 +0000

    add about

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,18 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+    get static_pages_home_url #static_pages/homeにアクセスしたら
+    assert_response :success  #レスポンスが成功するはず
   end
 
   test "should get help" do
-    get static_pages_help_url
-    assert_response :success
+    get static_pages_help_url  #static_pages/helpにアクセスしたら
+    assert_response :success  #レスポンスが成功するはず
+  end
+
+  test "should get about" do
+    get static_pages_about_url  #static_pages/aboutにアクセスしたら
+    assert_response :success  #レスポンスが成功するはず
   end
 
 end
