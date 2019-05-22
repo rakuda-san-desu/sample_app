2d5bc69debfa6ee3e95831766f86076dea4030a8
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-05-22 09:11:15 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,11 +6,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
-  test "should get home" do
-    get static_pages_home_url #static_pages/homeにアクセスしたら
-    assert_response :success  #レスポンスが成功するはず
-    assert_select "title", "Home | #{@base_title}"  #以下のHTMLが存在するはず
+  test "should get root" do
+    get root_url
+    assert_response :success
   end
+
 
   test "should get help" do
     get static_pages_help_url  #static_pages/helpにアクセスしたら
