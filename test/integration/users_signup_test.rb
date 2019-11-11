91bde128f62d49637fdea533354071814300ecae
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-11-11 17:29:41 +0900

    flashが空かどうかのテストをnil?からblank?に修正

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,6 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.nil?
+    assert_not flash.blank?
   end
 end
