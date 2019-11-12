981ba05000297b3df95d652f4abb7b00d2dd2517
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-11-12 17:37:03 +0900

    フラシュのテストをnil?からempty?に変更

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,6 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.blank?
+    assert_not flash.empty?
   end
 end
