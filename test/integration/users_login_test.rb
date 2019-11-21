9a32ebb754670688104d4f130785525f8f95b979
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-11-21 16:57:03 +0900

    ログインフォームの統合テスト追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "login with invalid information" do
+    # login_pathにgetのリクエスト
+    get login_path
+    #sessions/newが描写される
+    assert_template 'sessions/new'
+    #login_pathにpostのリクエスト　内容→params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: "", password: "" } }
+    #sessions/newが描写される
+    assert_template 'sessions/new'
+    #falseである　→flashはemptyか？
+    assert_not flash.empty?
+    #root_pathにgetのリクエスト
+    get root_path
+    #trueである　→flashはemptyか？
+    assert flash.empty?
+  end
 end
