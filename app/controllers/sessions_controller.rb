433f81e67493dd680ec7f706a8b25a63ee260354
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-11-21 17:24:58 +0900

    ログインエラーノflashメッセージを日本語化

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = t('.login_error')
       render 'new'
     end
   end
