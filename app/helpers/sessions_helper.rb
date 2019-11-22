71ba34961aac12f1d8294ce743c837887b81398b
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-11-22 19:28:05 +0900

    sessionsヘルパーにcurrent_userメソッドを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
 end
