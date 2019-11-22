f7846e4e439503741901de3b6a8d461485ac648d
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-11-22 19:26:26 +0900

    sessionsコントローラのcreateアクションにコメントを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      #session[:user_id] = user　と言う事
       log_in user
+      #user_url(user)　という名前付きルートになる
       redirect_to user
     else
       flash.now[:danger] = t('.login_error')
