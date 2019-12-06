ca7291aa13014f733cb96a8d8548f7f6e9cd7831
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-12-06 11:43:55 +0900

    test_helper.rbにis_logged_in?を定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      #sessions　helperで定義したlog_inメソッド
       log_in @user
       flash[:success] = t('.welcome_message')
       redirect_to @user
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,9 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    #sessionの:user_idがnilではない
+    !session[:user_id].nil?
+  end
 end
