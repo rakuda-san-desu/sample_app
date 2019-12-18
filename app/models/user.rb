7c16d7beb204c1f56f3cee11e7e54fb576d3f899
rakuda-san-desu <rakuda-san-desu@gmail.com>
2019-12-18 16:06:48 +0900

    ユーザーのログイン情報を破棄する

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,10 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
+  # ユーザーのログイン情報を破棄する
+  def forget
+    ##validationを無視して更新（:remember_digestの値をnilに）
+    update_attribute(:remember_digest, nil)
+  end
 end
