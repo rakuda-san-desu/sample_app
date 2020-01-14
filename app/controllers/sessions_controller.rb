56f6289eb176806113f08c97786285b1ba406e55
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-01-14 14:09:05 +0900

    ログインシステムの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #session[:user_id] = user　と言う事
       log_in user
-      #ログインしてユーザーを保持
-      remember user
+      #params[:session][:remember_me]が1の時userを記憶　そうでなければuserを忘れる
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       #user_url(user)　という名前付きルートになる
       redirect_to user
     else
