9ce42f7a8406af5371c7f77a8f0deaef2def3784
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-01-16 16:16:03 +0900

    永続的セッションのテストのコメント修正

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -11,13 +11,13 @@ class SessionsHelperTest < ActionView::TestCase
   test "current_user returns right user when session is nil" do
     # @userとcurrent_userが等しければtrue
     assert_equal @user, current_user
-    #テストユーザーがログインであればtrue
+    #テストユーザーがログイン中であればtrue
     assert is_logged_in?
   end
 
   #ユーザーの記憶ダイジェストが記憶トークンと正しく対応していない場合に現在のユーザーがnilになるか
   test "current_user returns nil when remember digest is wrong" do
-    #@userのremember_digestをUser.digest(User.new_token)の値に更新して保存
+    #@userのremember_digestを新しい値に更新して保存（setupと異なる値になるため現状のremember_tokenに対応）
     @user.update_attribute(:remember_digest, User.digest(User.new_token))
     #current_userがnilの時true
     assert_nil current_user
