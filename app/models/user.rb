ed6bb4e806d7ee3cc4d904898280f8ed0fcb9188
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-03-04 10:37:41 +0900

    downcase_emailメソッドの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ApplicationRecord
 
     # メールアドレスをすべて小文字にする
     def downcase_email
-      self.email = email.downcase
+      self.email.downcase!
     end
 
     # 有効化トークンとダイジェストを作成および代入する
