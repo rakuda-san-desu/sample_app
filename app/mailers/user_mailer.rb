04bff4583a3b3b86d065fdd7e8d83a8db96aaf8c
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-03-20 15:55:47 +0900

    メイラーのテスト

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,8 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     # インスタンス変数を定義
     @user = user
-    # user.emailにタイトルが"Account activation"のメールを送信
-    mail to: user.email, subject: "Account activation"
+    # user.emailにタイトルが"t('.account_activation_title')"のメールを送信
+    mail to: user.email, subject: t('.account_activation_title')
   end
 
   def password_reset
