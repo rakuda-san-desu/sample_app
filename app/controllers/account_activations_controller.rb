a2de7dd632399417612b1e52549bcdb32f70b175
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-04-02 02:08:41 +0900

    有効化のテストを追加

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,7 +3,7 @@ class AccountActivationsController < ApplicationController
   def edit
     # userに代入　→　Userテーブルから　URLから取得したemailの値を持つ　userデータを取得して
     user = User.find_by(email: params[:email])
-    # userが存在する　かつ　userがactivatedではない　かつ　有効化トークンとparams[:id]が持つ有効化ダイジェストが一致した場合
+    # userが存在する　かつ　userがactivatedではない　かつ　有効化ダイジェストとparams[:id](有効化トークン)が一致した場合
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       # userのactivatedの値をtrueに
       user.update_attribute(:activated,    true)
