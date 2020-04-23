8f9aeaa4d1e80912603b8d57896d4c5a6dc555a6
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-04-23 14:48:31 +0900

    アカウント再設定メールメソッドにコメント追加

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -6,7 +6,7 @@ class UserMailerPreview < ActionMailer::Preview
   def account_activation
     # userにDBの1番目のユーザーを代入
     user = User.first
-    # userのactivation_tokenに有効化トークンを代入
+    # userのactivation_tokenに新しい有効化トークンを代入
     user.activation_token = User.new_token
     # UserMailerクラスのaccount_activationメソッドを呼び出し（引数にuserを渡す）
     UserMailer.account_activation(user)
@@ -15,8 +15,11 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # https://5f7b98a0b66149149ecd6edabf11f3d6.vfs.cloud9.ap-northeast-1.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
+    # userにDBの一番目のユーザーを代入
     user = User.first
+    # userのreset_tokenに新しい有効化トークンを代入
     user.reset_token = User.new_token
+    # UserMailerクラスのpassword_resetメソッドを呼び出し（引数にuserを渡す）
     UserMailer.password_reset(user)
   end
 end
