cc7f0413e52ed930c26cbfb416b0433cfadfa8d2
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-06-30 13:41:04 +0900

    Forrow フォームとテストの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   get 'password_resets/new'
-
   get 'password_resets/edit'
 
   root 'static_pages#home'
@@ -15,12 +14,20 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  # resources :usersにネストを追加
+  resources :users do
+    # 以下でユーザーidが含まれるURLを扱えるようになる
+    member do
+      # :following, :followersのgetのルーティングを生成 
+      get :following, :followers
+    end
+  end
   # account_activationsresourceのeditへのルーティングのみを生成
   resources :account_activations, only: [:edit]
   # password_resetsのnew、create、edit、updateのルーティングを生成 
   resources :password_resets,     only: [:new, :create, :edit, :update]
   # micropostsのcreateとdestroyのルーティングを生成
   resources :microposts,          only: [:create, :destroy]
-
+  # relationshipsのcreateとdestroyのルーティングを生成
+  resources :relationships,       only: [:create, :destroy]
 end
