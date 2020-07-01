22899c28a43c5707e6644324776c3393a6da2dc0
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-07-01 16:12:52 +0900

    [Follow] ボタンの実装（Ajax）

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,20 +3,26 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    # userに代入　Userテーブルからparams[:followed_id]のデータを取得
-    user = User.find(params[:followed_id])
-    # current_userでuserをフォローする
-    current_user.follow(user)
-    # userのプロフィールページにリダイレクト
-    redirect_to user
+    # @userに代入　userテーブルからparams[:followed_id]のデータを取得
+    @user = User.find(params[:followed_id])
+    # current_userで@userをフォローする
+    current_user.follow(@user)
+    # リクエストの種類によってブロック内のいずれか1行が実行される
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    # userに代入　Relationshipテーブルからfollowedカラムの内容がparams[:id]のデータを取得
-    user = Relationship.find(params[:id]).followed
-    # current_userでuserをアンフォローする
-    current_user.unfollow(user)
-    # userのプロフィールページにリダイレクト
-    redirect_to user
+    # @userに代入　Relationshipテーブルからfollowedカラムの内容がparams[:id]のデータを取得
+    @user = Relationship.find(params[:id]).followed
+    # current_@serで@userをアンフォローする
+    current_user.unfollow(@user)
+    # リクエストの種類によってブロック内のいずれか1行が実行される
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
