b38b9af4f7f8b092a1d973b0a480264255d81a56
rakuda-san-desu <rakuda-san-desu@gmail.com>
2020-07-11 17:52:10 +0900

    フォロー機能のテストを追加

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class FollowingTest < ActionDispatch::IntegrationTest
-  # setupでmichaelを@userに代入ログイン済とする
+  # setupでmichaelを@userに代入、archerを@otherに代入しそれぞれログイン済とする
   def setup
     @user = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -32,6 +33,46 @@ class FollowingTest < ActionDispatch::IntegrationTest
     @user.followers.each do |user|
       # 特定のHTMLタグが存在する→ a href = "/users/userのid"
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+  
+  test "should follow a user the standard way" do
+    # ブロック内の処理の前後で@user.following.countが1増える
+    assert_difference '@user.following.count', 1 do
+      # relationships_pathにpostのリクエスト（@other をフォローする）
+      post relationships_path, params: { followed_id: @other.id }
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    # ブロック内の処理の前後で@user.following.countが1増える
+    assert_difference '@user.following.count', 1 do
+      # relationships_pathにAjaxでpostのリクエスト（@other をフォローする）
+      post relationships_path, xhr: true, params: { followed_id: @other.id }
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    # @userが@otherをフォロー
+    @user.follow(@other)
+    # relationshipに代入　→DBの@userのactive_relationshipsからfollowed_id:が@other.idと一致するデータ
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    # ブロック内の処理の前後で@user.following.countが-1
+    assert_difference '@user.following.count', -1 do
+      # relationship_pathにdeleteのリクエスト（relationshipを削除する）
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    # @userが@otherをフォロー
+    @user.follow(@other)
+    # relationshipに代入　→DBの@userのactive_relationshipsからfollowed_id:が@other.idと一致するデータ
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    # ブロック内の処理の前後で@user.following.countが-1
+    assert_difference '@user.following.count', -1 do
+      # relationship_pathAjaxでにdeleteのリクエスト（relationshipを削除する）
+      delete relationship_path(relationship), xhr: true
     end
   end
 end
